6e424ecd0c2d06c0bd28af5e0407560525d14df5
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-29 11:45:22 -0500

    commit tree a5185e94d40d813e0a12a91477d5c8eb89982563

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index 
+        students = Student.all 
+        render json: students
+    end
+    
+    def grades 
+        students_by_grade = Student.all.order(grade: :desc)
+        render json: students_by_grade
+    end
+    def highest_grade
+        students_by_highest_grade = Student.all.order(grade: :desc).first
+        render json: students_by_highest_grade
+    end
+end
